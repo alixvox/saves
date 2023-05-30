cebe344856119bbc416d11c18a92702a0dbd1967
Alex Leeper <alixvox@gmail.com>
2023-05-30 13:10:34 -0500

    Removing Files

--- CraftMap/run.py
+++
@@ -1,7 +0,0 @@
-from craftmap import app
-import webbrowser
-import threading
-
-if __name__ == '__main__':
-    threading.Timer(1.25, lambda: webbrowser.open('http://localhost:5000')).start()
-    app.run()
